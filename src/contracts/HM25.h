3a943e819ead778d78a6fb59dbd5c7653d2212ac
sabir.mba <sabir.mba@protonmail.com>
2025-03-22 17:46:42 +0100

    test changing contract with array and new

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -20,19 +20,38 @@ public:
         uint64 numberOfBurnCalls;
     };
 
+    struct GetStats_output
+    {
+    uint64 numberOfEchoCalls;
+    uint64 numberOfBurnCalls;
+
+    bool operator==(const GetStats_output& other) const
+    {
+        return numberOfEchoCalls == other.numberOfEchoCalls &&
+               numberOfBurnCalls == other.numberOfBurnCalls;
+    }
+
+    bool operator!=(const GetStats_output& other) const
+    {
+        return !(*this == other);
+    }
+    };
+
 private:
     uint64 numberOfEchoCalls;
     uint64 numberOfBurnCalls;
     QPI::Array<GetStats_output, 8> statsArray;
+    
     /**
     Send back the invocation amount
     */
     PUBLIC_PROCEDURE(Echo)
-        GetStats_output s = {100, 200};
-        statsArray.push_back(s);
+        GetStats_output stat;
+        stat.numberOfEchoCalls = 5;
+        stat.numberOfBurnCalls = 3;
         
-        state.numberOfEchoCalls = s.numberOfEchoCalls[0];
-
+        state.statsArray.set(0, stat);
+        state.numberOfEchoCalls = stat.numberOfEchoCalls;
         if (qpi.invocationReward() > 0)
         {
             qpi.transfer(qpi.invocator(), qpi.invocationReward());
