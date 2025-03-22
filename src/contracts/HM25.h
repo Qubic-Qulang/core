ac9ca67205a2f53cf4c3ecfbc0c438def536d359
sabir.mba <sabir.mba@protonmail.com>
2025-03-22 17:11:55 +0100

    test changing contract

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -28,7 +28,7 @@ private:
     Send back the invocation amount
     */
     PUBLIC_PROCEDURE(Echo)
-        state.numberOfEchoCalls++;
+        state.numberOfEchoCalls = 19;
         if (qpi.invocationReward() > 0)
         {
             qpi.transfer(qpi.invocator(), qpi.invocationReward());
