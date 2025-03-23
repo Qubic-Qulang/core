adbf88332707c68b84036eabe0ae8b2dd2906c82
RayanDri <rayan.drissi@icloud.com>
2025-03-23 02:37:50 +0100

    refactor HM25 contract: update GetUser function to set user ID using byte manipulation

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -448,7 +448,10 @@ private:
    PUBLIC_FUNCTION(GetUser)
    {
         id uid = id();
-        uid = m256i("0x12345678");
+        uid.setByteLE(0, 0x78);
+        uid.setByteLE(1, 0x56);
+        uid.setByteLE(2, 0x34);
+        uid.setByteLE(3, 0x12);
         uint64 balance = 56;
         User u = {uid, balance};
         state.users.set(0, u);
